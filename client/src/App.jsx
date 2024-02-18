334bb523749fa2c780a514d33a6cf7b9b9c2919c
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-02-19 02:25:04 +0530

    EF | Update Structure & Add BE Structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css';
+import { AdminDashboard, UserProfile, Login, SignUp} from './pages'
 import{BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
       <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/user" element={<UserProfile />} />
+          <Route path='/login' element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/admin/*" element={<AdminDashboard />} />
         </Routes>
       </BrowserRouter>
     </div>
