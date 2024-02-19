df2724692a41be340f9ada722141108e6fc93b14
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-02-20 01:42:56 +0530

    EF | setup basic routes and models

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ require('dotenv').config();
 const path = require('path');
 const app = express();
 
+const usersRoute = require("./routes/usersRoute");
+const bookingsRoute = require("./routes/bookingsRoute");
+const packagesRoute = require("./routes/packagesRoute");
+const employeesRoute = require("./routes/employeesRoute");
+
+
 app.use(express.json());
 
 const dbConfig = require("./db");
+
+app.use("/api/users", usersRoute);
+app.use("/api/bookings", bookingsRoute);
+app.use("/api/packages", packagesRoute);
+app.use("/api/employees", employeesRoute);
 
 const port = process.env.PORT || 5000;
 
