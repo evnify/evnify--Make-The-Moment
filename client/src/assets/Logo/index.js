a4eafa628812222078e3b4794207e12ebe405a5c
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-03-19 02:46:12 +0530

    EF | Adding initial Image asserts

---
+++ client/src/assets/Logo/index.js
@@ -0,0 +1,15 @@
+import Logo_black_with_tag from './Logo_black_with_tag.svg'
+import logo_black_without_tag from './logo_black_without_tag.svg'
+import logo_white_with_tag from './logo_white_with_tag.svg'
+import logo_white_without_tag from './Logo_white_without_tag.svg'
+import logo_dark_with_tag from './Logo_dark_with_tag.svg'
+import logo_dark_without_tag from './Logo_dark_without_tag.svg'
+
+export{
+    Logo_black_with_tag,
+    logo_black_without_tag,
+    logo_white_with_tag,
+    logo_white_without_tag,
+    logo_dark_with_tag,
+    logo_dark_without_tag
+}
---
+++ client/src/assets/index.js
@@ -0,0 +1,31 @@
+import {
+    Logo_black_with_tag,
+    logo_black_without_tag,
+    logo_dark_with_tag,
+    logo_dark_without_tag,
+    logo_white_with_tag,
+    logo_white_without_tag,
+} from "./Logo";
+import error_404 from "./404.svg";
+import error_500 from "./500.svg";
+import access_error from "./no_access.svg";
+import corosal01 from "./corosal01.svg";
+import corosal02 from "./corosal02.svg";
+import corosal03 from "./corosal03.svg";
+import corosal04 from "./corosal04.svg";
+
+export {
+    Logo_black_with_tag,
+    logo_black_without_tag,
+    logo_dark_with_tag,
+    logo_dark_without_tag,
+    logo_white_with_tag,
+    logo_white_without_tag,
+    error_404,
+    error_500,
+    access_error,
+    corosal01,
+    corosal02,
+    corosal03,
+    corosal04,
+};
