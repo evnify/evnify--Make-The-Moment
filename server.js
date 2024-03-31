4140d3c6e8e3e45e0d55df6d9b36cf06c15143ab
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-03-31 23:50:04 +0530

    EF | Heroku post build update

--- server.js
+++ server.js
@@ -33,6 +33,16 @@ app.use("/api/salary", salaryRoute);
 
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+    // Set the static folder
+    app.use(express.static("client/build"));
+
+    // Serve the index.html file for all non-API routes
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
 app.listen(port, () =>
     console.log(`Server running on port ${port} with nodemon`)
 );
