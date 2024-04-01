0b852e3e5e7d32f2bd0a91355ba98cd74dbf40b8
Nethmi De Silva <143713658+NethmiTharushi22@users.noreply.github.com>
2024-04-01 10:35:32 +0530

    EV | defined the path for the components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,14 +1,9 @@
 import {SideMenu, NavBar} from './admin'
 import AdminRoutes from './AdminRoutes'
-
-import { ChatBox, EmpRoutes, EmpSideMenu, AddBillingAddress ,BigImageView,SideMenuItems} from './users'
-
-export {SideMenu, AdminRoutes, NavBar, ChatBox, EmpRoutes, EmpSideMenu, AddBillingAddress,BigImageView,SideMenuItems}
 import EmpRoutes from './EmpRoutes'
 import ChatBox  from './ChatBox'
 import {EmpSideMenu, EmpHero} from "./employee";
 import UserRoutes from './UserRoutes';
-import { UserSideMenu } from './users';
+import { UserSideMenu,BigImageView,SideMenuItems, AddBillingAddress } from './users';
 
-export {SideMenu, AdminRoutes, NavBar, ChatBox, EmpRoutes, EmpSideMenu, EmpHero, UserRoutes, UserSideMenu}
-
+export {SideMenu, AdminRoutes, NavBar, ChatBox, EmpRoutes, EmpSideMenu, EmpHero, UserRoutes, UserSideMenu,BigImageView, AddBillingAddress,SideMenuItems}
--- client/src/components/users/index.js
+++ client/src/components/users/index.js
@@ -1,33 +1,22 @@
-
-import ChatBox from './ChatBox';
-import EmpRoutes from './EmpRoutes';
-import { EmpSideMenu } from './employee';
-import AddBillingAddress from './AddBillingAddress';
-import BigImageView from './BigImageView';
-import SideMenuItems from './SideMenuItems';
-
-export {
-    ChatBox,
-    EmpRoutes,
-    EmpSideMenu,
-    AddBillingAddress,
-    BigImageView,
-    SideMenuItems
-
 import UserSideMenu  from './UserSideMenu';
 import Navbar from './navBar';
 import Booking from './Booking';
 import Profile from './UserProfile';
 import UserSettings from './UserSettings';
 import ContactUS from './ContactUS';
-
+import AddBillingAddress from './AddBillingAddress';
+import BigImageView from './BigImageView';
+import SideMenuItems from './SideMenuItems';
 export {
     UserSideMenu,   
     Booking,
     Profile,
     Navbar,
     UserSettings,
-    ContactUS
+    ContactUS,
+    AddBillingAddress,
+    BigImageView,
+    SideMenuItems
 
- 
+
 }
