d4106d2b75986ff8cc7a2420c8f746a7c43e5c4c
Sasindu Nadeeshan <Sasindun2000@gmail.com>
2024-04-01 21:29:54 +0530

    EF | Email section
    Update Response to email
    finelize message

--- client/src/components/admin/AllMessages.jsx
+++ client/src/components/admin/AllMessages.jsx
@@ -1,15 +1,45 @@
 import React from "react";
-import { Input, Space, Tag } from 'antd';
+import { Input, Space, Tag, Select, Dropdown } from 'antd';
 import { messageDp } from "../../assets";
+import { DownOutlined } from '@ant-design/icons';
+import { Icon } from '@iconify/react';
 const { Search } = Input
 
+const items = [
+    {
+        label: <a>Read</a>,
+        key: '0',
+    },
+    {
+        label: <a>Unread</a>,
+        key: '1',
+    }
+];
 
 function AllMessages() {
     return <div>
         <div className="message-Box-all-messages">
             <div className="message-all-users-bar">
                 <div className="message-all-users-bar-top">
-
+                  <div style={{margin:"20px 0 0 50px"}}>
+                  <Dropdown
+                        menu={{
+                            items,
+                        }}
+                        trigger={['click']}
+                    >
+                        <a onClick={(e) => e.preventDefault()} style={{ color: '#000000', fontSize: '24px', fontWeight: 'bold'}}> {/* Change text color here */}
+                            <Space>
+                                Messages
+                                <DownOutlined />
+                            </Space>
+                        </a>
+                    </Dropdown>
+                  </div>
+                <div style={{margin:"30px 0 0 100px", fontSize:"12px"}}>
+                    filter by date
+                    <Icon icon="mingcute:filter-line" width="18" height="18" style={{margin:"0 0 0 5px"}}/>
+                </div>
                 </div>
                 <div className="message-all-users-bar-bottom">
                     <div className="message-all-users-bar-bottom-search">
@@ -85,17 +115,24 @@ function AllMessages() {
                     </div>
                     <div>
                         <div className="message-reply-admin-box">
-                            <div className="message-receved-admin">
-                                <img src={messageDp} alt="dp" style={{ width: "40px", height: "40px" }} />
-                                <div style={{ background: "#f1f1f1", borderRadius: "11px", margin: "0 0 0 15px", padding: "6px", maxWidth: "400px" }}>
-                                    <p>Hi, I'm Sasindu Nadeeshan. I'm interested in your product. Can you provide me more details about the product?</p>
+                            <div className="message-reply-admin-box-top">
+                                <div className="message-receved-admin">
+                                    <img src={messageDp} alt="dp" style={{ width: "40px", height: "40px" }} />
+                                    <div style={{ background: "#f1f1f1", borderRadius: "11px", margin: "0 0 0 15px", padding: "6px", maxWidth: "400px" }}>
+                                        <p>Hi, I'm Sasindu Nadeeshan. I'm interested in your product. Can you provide me more details about the product?</p>
+                                    </div>
+                                </div>
+                                <div className="message-send-admin">
+                                    <div style={{ background: "#7b63ff", borderRadius: "11px", margin: "0 15px 0 0", padding: "6px", maxWidth: "400px", color: "#ffffff" }}>
+                                        <p>Ask anything us</p>
+                                    </div>
+                                    <img src={messageDp} alt="dp" style={{ width: "40px", height: "40px" }} />
                                 </div>
                             </div>
-                            <div className="message-send-admin">
-                                <div style={{ background: "#7b63ff", borderRadius: "11px", margin: "0 15px 0 0", padding: "6px", maxWidth: "400px", color:"#ffffff"}}>
-                                    <p>Ask anything us</p>
-                                </div>
-                                <img src={messageDp} alt="dp" style={{ width: "40px", height: "40px" }} />
+                            <div className="message-Send-bar">
+                                <div style={{ margin: "5px 0 0 20px" }}><Icon icon="mingcute:emoji-line" width="24" height="24" /></div>
+                                <div style={{ margin: "2px 0 0 20px" }}><Input placeholder="Type Message" variant="borderless" style={{ width: 650 }} /></div>
+                                <div style={{ margin: "5px 0 0 20px" }}><Icon icon="majesticons:send" width="24" height="24" /></div>
                             </div>
                         </div>
                     </div>
--- client/src/components/admin/UserEmails.jsx
+++ client/src/components/admin/UserEmails.jsx
@@ -1,8 +1,53 @@
 import React from "react";
+import { messageDp } from "../../assets";
 
 function UserEmails() {
     return <div>
-        <h1>User Emails</h1>
+        <div className="email-full-container">
+            <div className="email-container-left">
+                <div className="left-side-full-email-top ">
+                    <b>All</b>
+                    <b>Read</b>
+                    <b>Unread</b>
+                </div>
+                <div className="left-side-full-email-down">
+                    <div className="left-side-full-email-view">
+                        <div><img src={messageDp} alt="dp" style={{ margin: "10px" }} /></div>
+                        <div className="left-side-full-email-view-text">
+                            <div className="name-email-view">
+                                <div>Sasindu Nadeeshan</div>
+                                <div style={{ margin: "0 0 0 60px" }}>01-02-24 14.22</div>
+                            </div>
+                            <div><p style={{ margin: "0 0 0 10px", fontWeight: "bold", fontSize: "20px" }}>What is want update</p></div>
+                            <div>
+                                <p style={{ fontSize: "12px", height: "20px", width: "300px", margin: "5px 0 0 10px", textAlign: "justify" }}>
+                                    Illustrations, and graphic elements from the world's best designers. Want more inspiration? Browse our search results.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="left-side-full-email-view">
+                        <div><img src={messageDp} alt="dp" style={{ margin: "10px" }} /></div>
+                        <div className="left-side-full-email-view-text">
+                            <div className="name-email-view">
+                                <div>Sasindu Nadeeshan</div>
+                                <div style={{ margin: "0 0 0 60px" }}>01-02-24 14.22</div>
+                            </div>
+                            <div><p style={{ margin: "0 0 0 10px", fontWeight: "bold", fontSize: "20px" }}>What is want update</p></div>
+                            <div>
+                                <p style={{ fontSize: "12px", height: "20px", width: "300px", margin: "5px 0 0 10px", textAlign: "justify" }}>
+                                    Illustrations, and graphic elements from the world's best designers. Want more inspiration? Browse our search results.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div></div>
+            </div>
+            <div className="email-container-right">
+
+            </div>
+        </div>
     </div>;
 }
 
