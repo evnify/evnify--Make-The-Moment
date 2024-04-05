e4c00253698c9ecb4ff3d65e200bc2e3cd66f137
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-04-05 15:30:35 +0530

    EF | Package page UI update
    Co-Authored-By: Nethmi De Silva <143713658+NethmiTharushi22@users.noreply.github.com>
    Co-Authored-By: N.P.Kaushani Dewmini <156335009+NPKaushaniDewmini@users.noreply.github.com>

--- client/src/pages/user/Packages.jsx
+++ client/src/pages/user/Packages.jsx
@@ -18,10 +18,13 @@ function Packages() {
                         eventType: category,
                     }
                 );
-                const selectedPackage = response.data.find(
-                    (packages) => packages.packageType === "basic"
+
+                setSelectedType(response.data);
+                const selectedPackages = response.data.find(
+                    (packages) => packages.packageType.toLowerCase() === "basic"
                 );
-                setSelectedPackage(selectedPackage);
+                setSelectedPackage(selectedPackages);
+                console.log(selectedPackages);
             } catch (error) {
                 console.log("Error fetching data", error);
             }
@@ -36,125 +39,163 @@ function Packages() {
         setSelectedPackage(clickedPackage);
     };
     const handleCreatePackage = () => {
-        // Redirect to packages page with selected package ID as URL parameter
         if (selectedPackage) {
             window.location.href = `/packages/${selectedPackage._id}`;
         }
     };
     return (
-        <div style={{ backgroundColor: "#E3E7EC" }}>
+        <>
             <Navbar />
-            <div className="packageTypeContainer_72">
-                {selectedType
-                    .filter((packages) => packages.eventType === category)
-                    .map((packages) => (
-                        <div key={packages._id}>
-                            <div
-                                className="user_package_type_card_72"
-                                style={{
-                                    backgroundImage: `linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0.4)),url(${packages.baseImage})`,
-                                }}
-                                onClick={() => handleImageClick(packages._id)}
-                            >
-                                <h2>{packages.packageType.toUpperCase()}</h2>
-                                <h2>PLAN</h2>
+            <div className="user_bookings_page_container">
+                <div className="packageTypeContainer_72">
+                    {selectedType
+                        .filter((packages) => packages.eventType === category)
+                        .map((packages) => (
+                            <div key={packages._id}>
+                                <div
+                                    className="user_package_type_card_72"
+                                    style={{
+                                        backgroundImage: `linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0.4)),url(${packages.baseImage})`,
+                                    }}
+                                    onClick={() =>
+                                        handleImageClick(packages._id)
+                                    }
+                                >
+                                    <h2>
+                                        {packages.packageType.toUpperCase()}
+                                    </h2>
+                                    <h2>PLAN</h2>
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
                     <div key="Custom">
-                            <div
-                                className="user_package_type_card_72"
-                                style={{
-                                    backgroundImage: `linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0.4)),url(https://s3-alpha-sig.figma.com/img/3ecd/1c36/311c9cdc92ca3d607e43420782f09e9e?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nhfmNAczjuKxUZPqwKhBK8PhcSqwFzYJ5-lr~o3gGHR81MVH~EMCrZgZCy0N5gkHcLPKBuYZZznhZMfGBUNqXBe7lFF9~9KB~GR1Dl3vvVI0s2Q~gpMuz3NyMonA0WTxbCXhTsGaNGnug1VYoj54A0--lrSE2K2zhtKU4UEthaAwLkMkdcMxKjg7SHzdWbb9FW8nLBqV51yZFYW9f8S7Z0Uk6LM8e-MtsHJVCWOidwU-F0pK0vlnBA-92cDxHdg1f-V4i2Lsw1gV4sjctyZfIZZMu~dUofCb7ZXMLQQ7NPW5Li02oHvNd4GnFNPUQ4dRk8V4Up7Rb5fJQTpOVuJkLA__)`,
-                                }}
-                            >
-                                <h2>CUSTOMIZE </h2>
-                                <h2>PLAN</h2>
-                            </div>
+                        <div
+                            className="user_package_type_card_72"
+                            style={{
+                                backgroundImage: `linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0.4)),url(https://s3-alpha-sig.figma.com/img/3ecd/1c36/311c9cdc92ca3d607e43420782f09e9e?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nhfmNAczjuKxUZPqwKhBK8PhcSqwFzYJ5-lr~o3gGHR81MVH~EMCrZgZCy0N5gkHcLPKBuYZZznhZMfGBUNqXBe7lFF9~9KB~GR1Dl3vvVI0s2Q~gpMuz3NyMonA0WTxbCXhTsGaNGnug1VYoj54A0--lrSE2K2zhtKU4UEthaAwLkMkdcMxKjg7SHzdWbb9FW8nLBqV51yZFYW9f8S7Z0Uk6LM8e-MtsHJVCWOidwU-F0pK0vlnBA-92cDxHdg1f-V4i2Lsw1gV4sjctyZfIZZMu~dUofCb7ZXMLQQ7NPW5Li02oHvNd4GnFNPUQ4dRk8V4Up7Rb5fJQTpOVuJkLA__)`,
+                            }}
+                        >
+                            <h2>CUSTOMIZE </h2>
+                            <h2>PLAN</h2>
                         </div>
-            </div>
-            <hr />
-            {selectedPackage &&
-                selectedPackage.inventories &&
-                selectedPackage.extras &&
-                selectedPackage.contentImages && (
-                    <div>
+                    </div>
+                </div>
+                <hr style={{ width: "80%" }} />
+                {selectedPackage &&
+                    selectedPackage.inventories &&
+                    selectedPackage.extras &&
+                    selectedPackage.contentImages && (
                         <div>
-                            <h2 style={{ fontSize: 36 }}>
-                                {" "}
-                                {selectedPackage.packageType} Plan
-                            </h2>
-                        </div>
-
-                        <div className="container">
-                            <div className="row ">
-                                <div className="selectedpackgeContainer_72">
-                                    <div>
-                                        {selectedPackage.contentImages.map(
-                                            (img, index) => (
-                                                <img
-                                                    key={index}
-                                                    alt={
-                                                        selectedPackage.packageType
-                                                    }
-                                                    src={img}
-                                                    style={{
-                                                        width: 100,
-                                                        height: 100,
-                                                        margin: 10,
-                                                        objectFit: "cover",
-                                                        borderRadius: 10,
-                                                        marginBottom: 20,
-                                                    }}
-                                                />
-                                            )
-                                        )}
+                            <div className="user_package_content_container">
+                                <h2>{selectedPackage.packageType} Plan</h2>
+                            </div>
+                            <div className="user_packages_main_section_container">
+                                <div className="user_packages_main_section">
+                                    <div className="user_packages_left_content">
+                                        <img
+                                            key={1}
+                                            alt={selectedPackage.packageType}
+                                            src={
+                                                selectedPackage.contentImages[0]
+                                            }
+                                            style={{
+                                                width: 250,
+                                                objectFit: "cover",
+                                            }}
+                                        />
+                                        <img
+                                            key={2}
+                                            alt={selectedPackage.packageType}
+                                            src={
+                                                selectedPackage.contentImages[0]
+                                            }
+                                            style={{
+                                                width: 250,
+                                                objectFit: "cover",
+                                            }}
+                                        />
+                                        <img
+                                            key={3}
+                                            alt={selectedPackage.packageType}
+                                            src={
+                                                selectedPackage.contentImages[0]
+                                            }
+                                            style={{
+                                                width: 250,
+                                                objectFit: "cover",
+                                            }}
+                                        />
+                                        <img
+                                            key={4}
+                                            alt={selectedPackage.packageType}
+                                            src={
+                                                selectedPackage.contentImages[0]
+                                            }
+                                            style={{
+                                                width: 250,
+                                                objectFit: "cover",
+                                            }}
+                                        />
+                                        <p
+                                            style={{
+                                                width: 220,
+                                                textAlign: "justify"
+                                            }}
+                                        >
+                                            {selectedPackage.description}
+                                        </p>
                                     </div>
-                                    <p style={{ width: 238 }}>
-                                        Description:{" "}
-                                        {selectedPackage.description}
-                                    </p>
-                                    <div className=" wrapCont_72 row ">
-                                        <div className="col-md-3 ">
-                                            <div className="packageOffer_72   ">
-                                                <h3>What You'll Get!</h3>
+                                    <div className="user_packages_right_content">
+                                        <div className="user_packages_right_content_card">
+                                            <h3>What You'll Get!</h3>
 
-                                                {selectedPackage.inventories.map(
-                                                    (item) => (
-                                                        <div>
-                                                            <ul>
-                                                                <li>
-                                                                    {item.count}{" "}
-                                                                    {item.name}s
-                                                                </li>
-                                                            </ul>
-                                                        </div>
-                                                    )
-                                                )}
-                                                <p>
-                                                    <b>
-                                                        LKR{" "}
-                                                        {selectedPackage.price}
-                                                    </b>
-                                                </p>
-                                            </div>
+                                            {selectedPackage.inventories.map(
+                                                (item) => (
+                                                    <div>
+                                                        <ul>
+                                                            <li>
+                                                                {item.count}{" "}
+                                                                {item.name}s
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                )
+                                            )}
+                                            <p>
+                                                <b>
+                                                    LKR {selectedPackage.price}
+                                                </b>
+                                            </p>
                                         </div>
-                                        <div className="col-md-3">
-                                            <div className="packageExtras_72">
-                                                <h3>Extras</h3>
-                                                {selectedPackage.extras.map(
-                                                    (item) => (
-                                                        <div>
-                                                            <ul>
-                                                                <li>{item}</li>
-                                                            </ul>
-                                                            <p></p>
-                                                        </div>
-                                                    )
-                                                )}
-                                            </div>
+                                        <div
+                                            className="user_packages_right_content_card"
+                                            style={{
+                                                backgroundColor: "#CEBEC9",
+                                            }}
+                                        >
+                                            <h3>Extras</h3>
+                                            {selectedPackage.extras.map(
+                                                (item) => (
+                                                    <div>
+                                                        <ul>
+                                                            <li>{item}</li>
+                                                        </ul>
+                                                        <p></p>
+                                                    </div>
+                                                )
+                                            )}
                                         </div>
                                     </div>
+                                </div>
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        flexDirection: "row",
+                                        justifyContent: "flex-end",
+                                        marginRight: 20,
+                                        marginBottom: 30,
+                                    }}
+                                >
                                     <button
                                         className="createPackageBtn_72 "
                                         onClick={handleCreatePackage}
@@ -164,9 +205,9 @@ function Packages() {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                )}
-        </div>
+                    )}
+            </div>
+        </>
     );
 }
 
