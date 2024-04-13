294baa51687d2f0038fdccdba14da8e231c4ebc1
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-04-13 11:03:52 +0530

    EF | Implement some admin access controls

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SideMenu, AdminRoutes, NavBar } from "../../components";
+import { useNavigate } from "react-router-dom";
 
 function AdminDashboard() {
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem("currentUser"));
+        if (!user) {
+            navigate("/login");
+        } else if (user.userType !== "Admin") {
+            navigate("/accessdenied");
+        } else {
+            setLoading(false);
+        }
+    }, []);
+
     return (
-        <div className="Admin_DashboardContainer">
-            <div className="Admin_SideMenuAndPageContent">
-                <div className="admin_sidebar_container">
-                    <SideMenu />
+        <>
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <div className="Admin_DashboardContainer">
+                    <div className="Admin_SideMenuAndPageContent">
+                        <div className="admin_sidebar_container">
+                            <SideMenu />
+                        </div>
+                        <div className="Admin_PageContent">
+                            <NavBar />
+                            <AdminRoutes />
+                        </div>
+                    </div>
                 </div>
-                <div className="Admin_PageContent">
-                    <NavBar />
-                    <AdminRoutes />
-                </div>
-            </div>
-        </div>
+            )}
+        </>
     );
 }
 
