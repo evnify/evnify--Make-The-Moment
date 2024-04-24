828a8d480bc51dcc95a135a32fc62d88af5d41ad
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2024-04-24 14:34:15 +0530

    EF | Insight structer
    Co-Authored-By: Niduka Konara <153778565+NidukaKonara@users.noreply.github.com>

--- client/src/components/admin/InventoryInsights.jsx
+++ client/src/components/admin/InventoryInsights.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 function InventoryInsights() {
-    return <div>
-        <h1>Inventory Insights</h1>
-        
-    </div>;
+    return (
+        <div className="inventory-insight">
+            <div className="inventory-insight-top">
+                <div className="inventory-insight-card">
+                    <p>Inventory</p>
+                </div>
+            </div>
+
+            <div className="inventory-insight-bottom">
+                <div className="inventroy-catagory"></div>
+                <div className="inventories"></div>
+            </div>
+        </div>
+    );
 }
 
 export default InventoryInsights;
