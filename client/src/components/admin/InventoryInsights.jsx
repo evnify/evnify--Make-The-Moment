dc01c4d01571c75ae59e8a9485af2bc770b55c7e
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2024-05-02 23:18:20 +0530

    Ef | Update Color
    Co-Authored-By: Niduka Konara <153778565+NidukaKonara@users.noreply.github.com>

--- client/src/components/admin/InventoryInsights.jsx
+++ client/src/components/admin/InventoryInsights.jsx
@@ -78,6 +78,7 @@ function InventoryInsights() {
                     "rgb(255, 99, 132)",
                     "rgb(54, 162, 235)",
                     "rgb(255, 205, 86)",
+                    "rgb(75, 192, 192)",
                 ],
                 hoverOffset: 4,
             },
