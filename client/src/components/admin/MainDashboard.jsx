7f9d2fb0c6148fb6f9ccad64439687aed7809237
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-05-02 16:00:31 +0530

    EF | View count charts and functionality

--- client/src/components/admin/MainDashboard.jsx
+++ client/src/components/admin/MainDashboard.jsx
@@ -1,21 +1,31 @@
 import React from "react";
+import ViewCountChart from "./ViewCountChart";
 
 function MainDashboard() {
     return (
-        <div className="admin_dashboard_card_main_section">
-            <div className="admin_dashboard_card card1">
-                <h1>Upcoming Events</h1>
-                <h2>10</h2>
-                <h3>4+ than last week</h3>
+        <div className="admin_dashboard_main_section">
+            <div className="admin_dashboard_card_main_section">
+                <div className="admin_dashboard_card card1">
+                    <h1>Upcoming Events</h1>
+                    <h2>10</h2>
+                    <h3>4+ than last week</h3>
+                </div>
+                <div className="admin_dashboard_card card2">
+                    <h1>Total Revenue</h1>
+                    <h2 style={{ fontSize: "32px" }}>54000 LKR</h2>
+                </div>
+                <div className="admin_dashboard_card card3">
+                    <h1>Upcoming Events</h1>
+                    <h2>10</h2>
+                    <h3>4+ than last week</h3>
+                </div>
+                <div className="admin_dashboard_card card4">
+                    <h1>Total Revenue</h1>
+                    <h2 style={{ fontSize: "32px" }}>54000 LKR</h2>
+                </div>
             </div>
-            <div className="admin_dashboard_card card2">
-                <h1>Total Revenue</h1>
-                <h2 style={{fontSize:"32px"}}>54000 LKR</h2>
-            </div>
-            <div className="admin_dashboard_card card3">
-                <h1>Upcoming Events</h1>
-                <h2>10</h2>
-                <h3>4+ than last week</h3>
+            <div className="admin_dashboard_chart_main_section">
+                <ViewCountChart />
             </div>
         </div>
     );
