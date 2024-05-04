ec42327598261e0c22b0b11fc536957d43264584
Sasindu Nadeeshan <Sasindun2000@gmail.com>
2024-05-04 11:41:23 +0530

    EF | Add payment Table
    Co-Authored-By: Supuni Samarathunga <145594033+sasuka-20@users.noreply.github.com>

--- client/src/components/admin/PaymentInsights.jsx
+++ client/src/components/admin/PaymentInsights.jsx
@@ -1,6 +1,7 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Line, Pie } from 'react-chartjs-2';
+import { Table } from 'antd';
 
 
 function PaymentInsights() {
@@ -20,7 +21,7 @@ function PaymentInsights() {
     useEffect(() => {
         getPaymentData();
     }, []);
-    
+
     //Line chart
 
     //Organize payment data by month
@@ -35,7 +36,7 @@ function PaymentInsights() {
     };
 
     const lineChartData = {
-        labels: ['January','February','March','April','May','June','July','August','September','October','November','December'], // Update labels to display the days of the week
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], // Update labels to display the days of the week
         datasets: [
             {
                 label: 'Payment In The Year',
@@ -53,26 +54,82 @@ function PaymentInsights() {
         }
     };
 
+    //payment table
+
+    const formattedPaymentData = paymentData.map(payment => {
+        const createdAtDate = new Date(payment.createdAt);
+        const formattedDate = createdAtDate.toLocaleString();
+
+        return {
+            ...payment,
+            date: formattedDate
+        };
+    });
+
+    const columns = [
+        {
+            title: "Transaction ID",
+            dataIndex: "transactionID",
+            key: "transactionID",
+        },
+        {
+            title: "Customer ID",
+            dataIndex: "customerID",
+            key: "customerID",
+        },
+        {
+            title: "Customer Email",
+            dataIndex: "customerEmail",
+            key: "customerEmail",
+        },
+        {
+            title: "Description",
+            dataIndex: "description",
+            key: "description",
+        },
+        {
+            title: "Amount",
+            dataIndex: "amount",
+            key: "amount",
+        },
+        {
+            title: "Date",
+            dataIndex: "date",
+            key: "date",
+        },
+    ];
+
+
     return <div>
         <div className="payment-insight-container">
             <div className="payment-insight">
                 <div className="insight-cards">
                     <div className="total-amount-card">
-                        <div style={{marginTop:"20px",fontSize:"18px",fontWeight:"bold"}}>Total Amount</div>
-                        <div style={{marginTop:"20px",fontSize:"32px",fontWeight:"bold"}}>{paymentData.reduce((acc, payment) => acc + payment.amount, 0)} LKR</div>
+                        <div style={{ marginTop: "20px", fontSize: "18px", fontWeight: "bold" }}>Total Amount</div>
+                        <div style={{ marginTop: "20px", fontSize: "32px", fontWeight: "bold" }}>{paymentData.reduce((acc, payment) => acc + payment.amount, 0)} LKR</div>
                     </div>
                     <div className="total-transaction-card">
-                        <div style={{marginTop:"20px",fontSize:"18px",fontWeight:"bold"}}>Transaction Count</div>
-                        <div style={{marginTop:"20px",fontSize:"32px",fontWeight:"bold"}}>{paymentData.length}</div>
+                        <div style={{ marginTop: "20px", fontSize: "18px", fontWeight: "bold" }}>Transaction Count</div>
+                        <div style={{ marginTop: "20px", fontSize: "32px", fontWeight: "bold" }}>{paymentData.length}</div>
                     </div>
                 </div>
                 <div className="insight-chart">
-                <Line data={lineChartData} options={options} width={280} height={100} />
+                    <Line data={lineChartData} options={options} width={280} height={100} />
                 </div>
             </div>
-            <div className="payment-table"></div>
+            <div className="payment-table-container">
+                <div>
+                    <div className="payment-table-top">
+                        <div className="payment-table-title">Payments</div>
+                        <div clssName="export-payment-btn">
+                            <button className="export-payment-csv">Export Csv</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="payment-table"><Table columns={columns} dataSource={formattedPaymentData} /></div>
+            </div>
         </div>
-        
+
     </div>;
 }
 
