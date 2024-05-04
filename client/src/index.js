a877354b6b36086c541fa78da57159b267fc1c43
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2024-05-04 20:49:51 +0530

    EF | Reset App.jsx

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js'
-import { GoogleOAuthProvider } from "@react-oauth/google"
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId='550101808419-h6glh8sc3bpfnkaq38963jjruntadf51.apps.googleusercontent.com'>
+
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-  </GoogleOAuthProvider>
+  
 
 );
 reportWebVitals();
